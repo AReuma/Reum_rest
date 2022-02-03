ccb6ac03581d9126017c8b8bbd50526cb69ebffc
sinareum <kuuniin@gmail.com>
2022-02-03 13:41:38 +0900

    수업 과제 복습

---
+++ Spring/2월 3일/demo/src/main/java/com/example/demo/entity/Member.java
@@ -0,0 +1,10 @@
+package com.example.demo.entity;
+
+import lombok.Data;
+
+@Data
+public class Member {
+
+    private String userName = "sdr4linux-eddi";
+    private String password = "12345678";
+}
